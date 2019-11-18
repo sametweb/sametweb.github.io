3a3417ce7ab2039ca001fdfb45caae43379ca137
sametweb <sametmutevellioglu@gmail.com>
2019-11-17 21:52:26 -0800

    Styling added, JS created for data

---
+++ index.js
@@ -0,0 +1,7 @@
+//prettier-ignore
+const social = [
+    { platform: 'github', icon: 'fa fa-github-square', link: 'https://github.com/sametweb' },
+    { platform: 'linkedin', icon: 'fa fa-linkedin-square', link: 'https://www.linkedin.com/in/samet-mutevelli/' },
+    { platform: 'twitter', icon: 'fa fa-twitter-square', link: 'https://twitter.com/sametmutevelli' },
+    { platform: 'facebook', icon: 'fa fa-facebook-square', link: 'https://www.facebook.com/sametmutevelli' }
+];
